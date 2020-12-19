20dd776a7c32cefb7aa92cbdfc0061372aaf1f84
Xiu_Er <13813641925@163.com>
2020-12-19 19:16:46 +0800

    1.创建redis项目 2.升级springboot版本 2.2.12 RELEASE

---
+++ SpringBoot-Redis/src/test/java/com/ovopark/tao/redis/RedisApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ovopark.tao.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RedisApplicationTest {
+
+    @Test
+    void t(){
+
+    }
+}
